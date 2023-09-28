a5eeee53541c9c202278a525361f9a0f652f3285
Cheprichate <petrovich15071998@mail.ru>
2023-09-28 20:55:03 +0400

    Добавьте файлы проекта.

---
+++ ProgressivTAX.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+int pay;
+int tax;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "ВВедите заработанную сумму : \n";
+    std::cin >> pay;
+
+   if (pay > 50000) {
+        tax += ( pay - 50000 ) * 30 / 100 ;
+        pay = 50000;
+   }
+   if (pay > 10000) {
+       tax += ( pay - 10000 ) * 20 / 100 ;
+       pay = 10000;
+   }
+       tax += pay * 13 / 100;
+
+    std::cout << "Налог составит :"<< tax << "\n";
+}
